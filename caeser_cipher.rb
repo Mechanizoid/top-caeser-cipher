8a3e1293d25aebf7931afecb19371cf646b45562
Christopher Phoenix <hello@chrisphoenix.dev>
2026-08-26 16:17:13 -0700

    feat: implement Caeser shift cipher method

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,6 +1,27 @@
 # TOP Project: Caeser Cipher
 
-# TODO: implement Caeser shift cipher
-def caeser_cipher(string, n)
+def caeser_cipher(string, shift)
+  plaintext_array = string.split('')
 
+  ciphertext_array = plaintext_array.map do | character |
+    if /[a-z]/.match?(character)
+      offset = 97
+      character = ((((character.ord - offset) + shift) % 26) + offset).chr
+    elsif /[A-Z]/.match?(character)
+      offset = 65
+      character = ((((character.ord - offset) + shift) % 26) + offset).chr
+    end
+
+    character
+  end
+
+  ciphertext_array.join('')
 end
+
+# Begin script
+print "Enter your plaintext: "
+input_string = gets.chomp
+print "Enter your desired shift (0..26): "
+shift = gets.chomp.to_i
+
+puts "\nCiphertext:\n#{caeser_cipher(input_string, shift)}"
