580294dad49121bcdd60a5e08493812eb09c65c7
Christopher Phoenix <hello@chrisphoenix.dev>
2026-09-25 11:51:34 -0700

    style: run rubocop autocorrect for style issues

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -3,19 +3,21 @@
 def shift_character(character, shift)
   offset = /[A-Z]/.match?(character) ? 65 : 97
 
-  /[A-Za-z]/.match?(character) ?
-    ((((character.ord - offset) + shift) % 26) + offset).chr :
+  if /[A-Za-z]/.match?(character)
+    ((((character.ord - offset) + shift) % 26) + offset).chr
+  else
     character
+  end
 end
 
 def caeser_cipher(string, shift)
-  string.chars.map { | character | shift_character(character, shift) }.join
+  string.chars.map { |character| shift_character(character, shift) }.join
 end
 
 if __FILE__ == $0
-  print "Enter your plaintext: "
+  print 'Enter your plaintext: '
   input_string = gets.chomp
-  print "Enter your desired shift (0..26): "
+  print 'Enter your desired shift (0..26): '
   shift = gets.chomp.to_i
 
   puts "\nCiphertext:\n#{caeser_cipher(input_string, shift)}"
